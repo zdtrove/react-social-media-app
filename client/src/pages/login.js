2d9e4f47ec2e96bda3b6ca74bfe67d6677cdd6b9
Hoang An <antnh@icd-vn.com>
2021-06-03 16:15:57 +0700

    cm

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -29,44 +29,53 @@ const Login = () => {
 	return (
 		<div className="auth-page">
 			<form onSubmit={handleSubmit}>
-				<h3 className="mb-4 text-center text-uppercase">Social Media App</h3>
-				<div className="form-group">
-					<label htmlFor="exampleInputEmail1">Email address</label>
-					<input
-						type="email"
-						className="form-control"
-						d="exampleInputEmail1"
-						aria-describedby="emailHelp"
-						name="email"
-						value={email}
-						onChange={handleChangeInput}
-					/>
-					<small className="form-text text-muted">
-						We'll never share your email with anyone else.
-					</small>
-				</div>
-				<div className="form-group">
-					<label htmlFor="exampleInputPassword1">Password</label>
-					<div className="pass">
-						<input
-							type={typePass ? "text" : "password"}
-							className="form-control"
-							id="exampleInputPassword1"
-							name="password"
-							value={password}
-							onChange={handleChangeInput}
-						/>
-						<small onClick={() => setTypePass(!typePass)}>
-							{typePass ? 'Hide' : 'Show'}
-						</small>
+				<div className="bg-img">
+					<div className="content">
+						<div className="image-login"></div>
+						<div className="field">
+							<span className="fa fa-user"></span>
+							<input
+								required
+								placeholder="Email"
+								type="email"
+								name="email"
+								value={email}
+								onChange={handleChangeInput}
+							/>
+						</div>
+						<div className="field space">
+							<span className="fa fa-lock"></span>
+							<input
+								className="pass-key"
+								required
+								placeholder="Password"
+								type={typePass ? "text" : "password"}
+								name="password"
+								value={password}
+								onChange={handleChangeInput}
+							/>
+							<span onClick={() => setTypePass(!typePass)} className="show">
+								{typePass ? 'Hide' : 'Show'}
+							</span>
+						</div>
+						<hr />
+						<div className="field">
+							<input disabled={email && password ? false : true} className="submit" type="submit" value="LOGIN" />
+						</div>
+						<div className="login">Or login with</div>
+						<div className="links">
+							<div className="facebook">
+								<i className="fab fa-facebook-f"><span>Facebook</span></i>
+							</div>
+							<div className="instagram">
+								<i className="fab fa-instagram"><span>Instagram</span></i>
+							</div>
+						</div>
+						<div className="signup">
+							You don't have an account? <Link style={{ color: "crimson" }} to="/register">Register Now</Link>
+						</div>
 					</div>
 				</div>
-				<button disabled={email && password ? false : true} type="submit" className="btn btn-dark w-100">
-					Login
-				</button>
-				<p className="my-2">
-					You don't have an account? <Link style={{ color: "crimson" }} to="/register">Register Now</Link>
-				</p>
 			</form>
 		</div>
 	)
